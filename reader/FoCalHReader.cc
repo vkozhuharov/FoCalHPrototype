1d57b623a72e5b94ce09e43d0e5f1355a5d8f2e5
Venelin Kozhuharov <vkozhuharov@gmail.com>
2021-10-04 14:04:02 +0200

    Removed the config structure and place it in a separate place

--- reader/FoCalHReader.cc
+++ reader/FoCalHReader.cc
@@ -21,6 +21,9 @@
 #define TIMEWINDOW 300 //500 ns time window for event matching
 #define TS1LIMIT 10
 
+
+#include "ProtoFoCalHConfig.hh"
+
 #include "pedestals.h"
 
 //TO BE DONE
@@ -44,69 +47,6 @@ void sighandler(int sig){
 
     exit(0);
 }
-
-class ProtoFoCalHConfig {
-public:
-  void ReadConfig();
-  void PrintConfig();
-  int GetX(int mac,int ich){return X[mac][ich];};
-  int GetY(int mac,int ich){return Y[mac][ich];};
-  //For the moment MAC addresses go consequitively
-  
-public:
-  int X[2][32];
-  int Y[2][32];
-  int ncards;  
-};
-
-void ProtoFoCalHConfig::PrintConfig(){
-  std::cout << "====== FoCal test configuration =====" << std::endl;
-  for(int imac = 0;imac<ncards;imac++) {
-    for(int ich = 0;ich < 32;ich++){
-      std::cout << imac << "   " << ich << "    " << X[imac][ich] << "   " << Y[imac][ich] << std::endl;
-    }
-  }
-
-}
-
-
-void ProtoFoCalHConfig::ReadConfig(){
-  std::ifstream filein ("FoCal.conf", std::ifstream::in);
-  std::string line;
-
-  //Get the first non-comment line
-  //Should contain the number of cards
-  for (std::string line; std::getline(filein, line); ){
-    std::cout << line << std::endl;
-    if(line.find("#") == 0) {
-      continue;
-    } else {
-      std::stringstream ss(line);
-      ss >> ncards;
-      break;
-    }
-  }
-
-  //Book space for the channels mapping
-  //Leave it for the moment --TO BE DONE--
-  //
-
-  int mac; int ch; int x; int y;
-  for (std::string line; std::getline(filein, line); ){
-    std::cout << line << std::endl;
-    if(line.find("#") == 0) continue;
-    std::stringstream ss(line);
-    ss >> mac >> ch >> x >> y;
-    
-    if(mac >= 0 && ch >= 0 && ch < 32) {
-      X[mac][ch] = x;
-      Y[mac][ch] = y;
-    } else {
-      
-    }
-  }
-}
-
 
 
 
