9b8d020d12c7eb3c0deb318d7c17ad3c2628914f
Venelin Kozhuharov <vkozhuharov@gmail.com>
2021-10-12 19:08:44 +0300

    Separating the main function from the rest of the code 	to make it easily portable to ROOT CLING interpreter

---
+++ reader/FoCalH.cc
@@ -0,0 +1,78 @@
+#include <signal.h>
+#include <iostream>
+#include "HistoSvc.hh"
+
+
+int FoCalHReader(char* ,char *);
+
+
+void usage(char *name){
+  std::cout << name << "  -l board_with_MAC_0   -r  board_with_MAC_1" << std::endl;
+  return;
+}
+
+void sighandler(int sig){
+    std::cerr << std::endl << "********************************************************************************" << std::endl;
+    std::cerr << "Killed with Signal " << sig << std::endl << "Closing ROOT files ..." << std::endl;
+
+    HistoSvc* histos = HistoSvc::GetInstance();
+    histos->HistoExit();
+
+    std::cerr << "... Histogram file Done" << std::endl;
+    std::cerr << std::endl << "********************************************************************************" << std::endl;
+
+    exit(0);
+}
+
+
+int main(int argc, char **argv){
+
+  signal(SIGXCPU,sighandler);
+  signal(SIGINT,sighandler);
+  signal(SIGTERM,sighandler);
+  signal(127,sighandler);
+  
+
+   char b0FileName[256];
+   char b1FileName[256];
+
+  int ne = 0;
+  int nopt;
+  int opt;
+
+  int nl=0;
+  int nr=0;
+  int nn=0;
+  int nEventsToRead;
+  
+  extern char *optarg;
+
+  while ((opt = getopt(argc, argv, "l:r:n:")) != -1) {
+    switch (opt) {
+      case 'l':
+	strncpy(b0FileName,optarg,250);
+	nl++;
+        break;
+	
+      case 'r':
+	strncpy(b1FileName,optarg,250);
+	nr++;
+        break;		
+    default:
+      break;
+      usage(argv[0]);
+      return 0;
+    }
+  }
+
+  if( nl!=1 || nr!=1) {
+    usage(argv[0]);
+    return 0;
+  }
+  std::cout << "Using " << b0FileName << " with MAC5=0  and "<<b1FileName << " with MAC5=1 " << std::endl;
+  
+  FoCalHReader(b0FileName, b1FileName);
+  
+  return 0;
+
+}
--- reader/FoCalHReader.cc
+++ reader/FoCalHReader.cc
@@ -4,7 +4,6 @@
 #include <unistd.h>
 #include <iostream>
 #include <fstream>      // std::ifstream
-#include <signal.h>
 #include <sstream>
 
 #include "TFile.h"
@@ -30,23 +29,6 @@
 
 //matching between two files with different trees
 
-void usage(char *name){
-  std::cout << name << "  -l board_with_MAC_0   -r  board_with_MAC_1" << std::endl;
-  return;
-}
-
-void sighandler(int sig){
-    std::cerr << std::endl << "********************************************************************************" << std::endl;
-    std::cerr << "Killed with Signal " << sig << std::endl << "Closing ROOT files ..." << std::endl;
-
-    HistoSvc* histos = HistoSvc::GetInstance();
-    histos->HistoExit();
-
-    std::cerr << "... Histogram file Done" << std::endl;
-    std::cerr << std::endl << "********************************************************************************" << std::endl;
-
-    exit(0);
-}
 
 
 
@@ -131,55 +113,10 @@ int fillEventSingleBoard(struct ProtoFoCalHBoard *bevt, ProtoFoCalHEvent *event,
 
 }
 
-int main(int argc, char **argv){
+int FoCalHReader(char* b0FileName,char *b1FileName){
 
-  signal(SIGXCPU,sighandler);
-  signal(SIGINT,sighandler);
-  signal(SIGTERM,sighandler);
-  signal(127,sighandler);
-  
   ProtoFoCalHEvent event;
 
-   char b0FileName[256];
-   char b1FileName[256];
-
-  int ne = 0;
-  int nopt;
-  int opt;
-
-  int nl=0;
-  int nr=0;
-  int nn=0;
-  int nEventsToRead;
-  
-  extern char *optarg;
-
-  while ((opt = getopt(argc, argv, "l:r:n:")) != -1) {
-    switch (opt) {
-      case 'l':
-	strncpy(b0FileName,optarg,250);
-	nl++;
-        break;
-	
-      case 'r':
-	strncpy(b1FileName,optarg,250);
-	nr++;
-        break;		
-    default:
-      break;
-      usage(argv[0]);
-      return 0;
-    }
-  }
-
-  if( nl!=1 || nr!=1) {
-    usage(argv[0]);
-    return 0;
-  }
-  std::cout << "Using " << b0FileName << " with MAC5=0  and "<<b1FileName << " with MAC5=1 " << std::endl;
-
-  //return 0;
-  
   //int FoCalRead(){
   TFile *f = new TFile(b0FileName);
   TTree *t1 = (TTree*)f->Get("mppc");
